bc79df52428f342b731892b2d1ad044f22b342c8
shlcell <1285067785@qq.com>
2023-03-14 22:11:05 +0800

    新增流测试类

---
+++ test-other/src/main/java/com/shl/demo/vo/Pool.java
@@ -0,0 +1,30 @@
+package com.shl.demo.vo;
+
+import lombok.Data;
+
+@Data
+public class Pool {
+    private String a;
+    private int b;
+
+    public Pool(String a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+}
