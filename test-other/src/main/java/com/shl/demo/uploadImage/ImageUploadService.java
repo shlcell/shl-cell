9a14987765a72dbe823371dc0df5c9d50296e429
shlcell <1285067785@qq.com>
2023-04-06 23:03:02 +0800

    修改图片校验

--- test-other/src/main/java/com/shl/demo/uploadImage/ImageUploadService.java
+++ test-other/src/main/java/com/shl/demo/uploadImage/ImageUploadService.java
@@ -90,7 +90,30 @@ public class ImageUploadService {
             return false;
         }
 
+/*
+try-with-resources语句是 Java 7 引入的一种新的流程控制结构。它可以让开发者更加方便地管理资源，并且使代码更加简洁易读。
+在传统的try-catch-finally结构中，我们需要显式地关闭资源，如文件、数据库连接、网络连接等。
+而在使用try-with-resources结构时，只需要在try关键字后面的括号中声明要使用的资源，Java 会自动在代码块执行完毕后关闭这些资源，从而避免了繁琐的释放资源操作。
+
+ImageInputStream和BufferedImage都是Java图像处理领域中的常用类，但是它们的功能和应用场景有所不同。
+
+ImageInputStream是一个可寻址的输入流接口，用于读取图片数据以及元数据。
+它继承自Java标准库中的DataInput接口和Closeable接口，可支持基本的数据操作和关闭资源等操作。
+很多图像处理库和工具使用ImageInputStream读取图片数据并解码成图像对象，例如Java自带的ImageIO类就使用了ImageInputStream来读取和处理图片文件。
+
+BufferedImage是一个具体的图像缓冲区类，它描述了一个包含图像数据的可访问缓冲区。
+一个BufferedImage对象由图像的颜色模型和像素数据组成，它可以更加灵活地操作图片数据、生成新的图像、裁剪和缩放等常见图像处理操作。
+在日常的Java图像处理开发中，我们通常会将BufferedImage作为图像对象进行创建、修改和保存等操作，例如使用Graphics2D类在BufferedImage上绘制图形或者转换图片格式等。
+
+因此， ImageInputStream是一个读取和解码图片数据的工具类，而BufferedImage则是一个用于描述和处理图像缓冲区的类，它们在图像处理中有着不同的作用和应用场景。
+*/
         try (ByteArrayInputStream in = new ByteArrayInputStream(imageBytes)) {
+            BufferedImage image = ImageIO.read(in);
+            int width = image.getWidth();
+            int height = image.getHeight();
+            if (width > WIDTH_LIMIT || height > HEIGHT_LIMIT) {
+                return false;
+            }
             ImageInputStream iis = ImageIO.createImageInputStream(in);
 //            getFormatName()是BufferedImage类中的方法，但是需要注意的是该方法在Java SE 9中被弃用，如果您使用的是Java SE 9及以上版本，则建议使用ImageReader类来获取图像格式
 //            String formatName = ImageIO.read(in).getFormatName().toLowerCase();
